ca3727bf81c8c1103b8475c74af4e346c512102d
XXairship <116432782+ipx13@users.noreply.github.com>
2023-01-27 17:43:15 +0800

    Completed the bubble sort algorithm

---
+++ bubbleSort.py
@@ -0,0 +1,11 @@
+def sort(set_data):
+    for x in range(set_size):
+        for y in range(0, (set_size - x) - 1):
+            if set_data[y] > set_data[y + 1]:
+                set_data[y], set_data[y + 1] = set_data[y + 1], set_data[y]
+
+set_data = [100, 49, 55, 90, 47, 29, 97, 39, 42, 44]
+set_size = len(set_data)
+
+sort(set_data)
+print(set_data)
